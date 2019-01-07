81c70fad534c9e54d2f7d52d38cdb0ad7d7fd58c
luheng <1040622633@qq.com>
2019-01-07 13:40:59 +0800

    一

---
+++ zhouer/zhu.php
@@ -0,0 +1,30 @@
+<div>
+	<form action="?r=zhouer/tian" method="post">
+		<table border="1">
+			<tr>
+				<td>用户名：</td>
+				<td><input type="text" name="yong"></td>
+			</tr>
+			<tr>
+				<td>密码：</td>
+				<td><input type="password" name="mi"></td>
+			</tr>
+			<tr>
+				<td>确认密码：</td>
+				<td><input type="password"></td>
+			</tr>
+			<tr>
+				<td>E-mail：</td>
+				<td><input type="text" name="mail">邮箱和手机至少填一项</td>
+			</tr>
+			<tr>
+				<td>手机：</td>
+				<td><input type="text" name="shou">邮箱和手机至少填一项</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" value="完成"></td>
+			</tr>
+		</table>
+	</form>
+</div>
